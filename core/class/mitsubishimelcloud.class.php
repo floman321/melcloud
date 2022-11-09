90bd50a6a4130564e66b6d5053e4388ceb35626d
DuchkPy <valjean1@laposte.net>
2022-11-09 13:43:28 +0100

    Update SetTemperature subtype

--- core/class/mitsubishimelcloud.class.php
+++ core/class/mitsubishimelcloud.class.php
@@ -483,6 +483,30 @@ class mitsubishimelcloud extends eqLogic {
           $SetTemperature_Value->save();
         }
         
+        // $i++;
+        // $SetTemperature = $this->getCmd(null, 'SetTemperature');
+        // if(!is_object($SetTemperature)) {
+        //   $SetTemperature = (new mitsubishimelcloudCmd)
+        //   ->setName(__('Température consigne', __FILE__))
+        //   ->setLogicalId('SetTemperature')
+        //   ->setOrder($i)
+        //   ->setIsVisible(1)
+        //   ->setIsHistorized(0)
+        //   ->setType('action')
+        //   ->setSubType('slider')
+        //   ->setConfiguration('minValue', 10)
+        //   ->setConfiguration('maxValue', 30)
+        //   ->setConfiguration('step', 1)
+        //   ->setUnite('°C')
+        //   ->setTemplate('dashboard', 'TemperatureMitsubishi')
+        //   ->setTemplate('mobile', 'TemperatureMitsubishi')
+        //   ->setDisplay('generic_type', 'THERMOSTAT_SETPOINT')
+        //   ->setConfiguration('updateCmdId', $SetTemperature_Value->getEqLogic_id())
+        //   ->setValue($SetTemperature_Value->getId())
+        //   ->setEqLogic_id($this->getId());
+        //   $SetTemperature->save();
+        // }
+        
         $i++;
         $SetTemperature = $this->getCmd(null, 'SetTemperature');
         if(!is_object($SetTemperature)) {
@@ -493,10 +517,8 @@ class mitsubishimelcloud extends eqLogic {
           ->setIsVisible(1)
           ->setIsHistorized(0)
           ->setType('action')
-          ->setSubType('slider')
-          ->setConfiguration('minValue', 10)
-          ->setConfiguration('maxValue', 30)
-          ->setConfiguration('step', 1)
+          ->setSubType('select')
+          ->setConfiguration('listvalue', '10|10; 11|11; 12|12; 13|13; 14|14; 15|15; 16|16; 17|17; 18|18; 19|19; 20|20; 21|21; 22|22; 23|23; 24|24; 25|25; 26|26; 27|27; 28|28; 29|29; 30|30')
           ->setUnite('°C')
           ->setTemplate('dashboard', 'TemperatureMitsubishi')
           ->setTemplate('mobile', 'TemperatureMitsubishi')
